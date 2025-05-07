c32e66fdd98637a3a83078d7b8ca8c421db301e5
Shanto93 <shantoislam7363@gmail.com>
2025-05-07 20:56:00 +0600

    Implement solution for Problem 7

--- module1/src/assignments/problem6.ts
+++ module1/src/assignments/problem6.ts
@@ -1,25 +1,28 @@
-interface Product {
-  name: string;
-  price: number;
-}
-
-function getMostExpensiveProduct(products: Product[]): Product | null {
-  if (products.length === 0) {
-    return null;
+{
+  //Problem 6
+  interface Product {
+    name: string;
+    price: number;
   }
-  let mostExpensiveProduct = products[0];
-  for (let i = 1; i < products.length; i++) {
-    if (products[i].price > mostExpensiveProduct.price) {
-      mostExpensiveProduct = products[i];
+
+  function getMostExpensiveProduct(products: Product[]): Product | null {
+    if (products.length === 0) {
+      return null;
     }
+    let mostExpensiveProduct = products[0];
+    for (let i = 1; i < products.length; i++) {
+      if (products[i].price > mostExpensiveProduct.price) {
+        mostExpensiveProduct = products[i];
+      }
+    }
+    return mostExpensiveProduct;
   }
-  return mostExpensiveProduct;
+
+  const products = [
+    { name: "Pen", price: 10 },
+    { name: "Notebook", price: 25 },
+    { name: "Bag", price: 50 },
+  ];
+
+  getMostExpensiveProduct(products);
 }
-
-const products = [
-  { name: "Pen", price: 10 },
-  { name: "Notebook", price: 25 },
-  { name: "Bag", price: 50 },
-];
-
-getMostExpensiveProduct(products);
---
+++ module1/src/assignments/problem7.ts
@@ -0,0 +1,22 @@
+{
+  //Problem 7
+  enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+  }
+
+  function getDayType(day: Day): string {
+    if (day === Day.Saturday || day === Day.Sunday) {
+      return "Weekend";
+    } else {
+      return "Weekday";
+    }
+  }
+
+  getDayType(Day.Friday);
+}
